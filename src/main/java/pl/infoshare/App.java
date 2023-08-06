37caf00069a386d9650b06b2cf301459910f4676
Roman Kachniuk <roman.kachniuk@gmail.com>
2023-08-06 12:31:55 +0200

    added: Apache Commons lang3

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,13 +1,12 @@
 package pl.infoshare;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.commons.lang3.StringUtils;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println(StringUtils.endsWith("slowo","!!!"));
+
     }
 }
